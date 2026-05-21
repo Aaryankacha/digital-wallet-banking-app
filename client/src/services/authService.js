14e2234e93c2b0b2665ff36d6ed9b5ded0edc3f9
Aryan <175712247+Aaryankacha@users.noreply.github.com>
2026-05-21 11:55:40 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,7 @@
 import api from './api';
 
 /** Register a new user */
-export const registerUser = (data) => api.post('/auth/register', data);
+export const registerUser = (data) => api.post('/api/register', data);
 
 /** Login – returns { token, user } */
-export const loginUser = (data) => api.post('/auth/login', data);
+export const loginUser = (data) => api.post('/api/login', data);
