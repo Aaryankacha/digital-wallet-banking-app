bb22806349c6d28230c9bdfe5aa4dea1e1f757be
Aryan <175712247+Aaryankacha@users.noreply.github.com>
2026-05-21 12:33:30 +0530

    Update contactService.js

--- client/src/services/contactService.js
+++ client/src/services/contactService.js
@@ -1,10 +1,10 @@
 import api from './api';
 
 /** Retrieve contact list for logged-in user */
-export const getContacts = () => api.get('/contacts');
+export const getContacts = () => api.get('/api/contacts');
 
 /** Add a new contact */
-export const addContact = (data) => api.post('/contacts', data);
+export const addContact = (data) => api.post('/api/contacts', data);
 
 /** Remove a contact by its ID */
-export const removeContact = (id) => api.delete(`/contacts/${id}`);
+export const removeContact = (id) => api.delete(`/api/contacts/${id}`);
