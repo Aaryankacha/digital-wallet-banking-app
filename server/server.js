7e06754fa88bf6f18042b365864fff353296bf9a
Aryan <175712247+Aaryankacha@users.noreply.github.com>
2026-05-22 19:13:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'https://digital-wallet-banking-app-uh23.vercel.app',
+    'https://paywave-wallet.vercel.app',
     'http://localhost:5173'
   ],
   credentials: true,
